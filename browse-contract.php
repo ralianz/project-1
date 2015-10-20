31fb304516a0fc072f6ff791625a8760fed719d7
ralianz <ra.lianze@gmail.com>
2015-10-20 22:53:56 +0700

    versi 1.0.0

---
+++ browse-contract.php
@@ -0,0 +1,381 @@
+<!doctype HTML>
+<html lang="en">
+
+<head>
+	
+	<meta charset="UTF-8">
+	<title>Privydoc</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<link rel="stylesheet" href="css/bootstrap.css" media="screen">
+	<link rel="stylesheet" href="css/bootstrap.min.css" media="screen">
+	<link rel="stylesheet" href="css/style.css" media="screen">
+	<link href='http://fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Roboto:100' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Roboto:500' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Exo+2:400,300' rel='stylesheet' type='text/css'>
+	<link rel="shortcut icon" href="assets/image/favicon.ico" sizes="16x16" />
+	<script src="js/jquery.js"></script>
+	<script src="js/jquery.min.js"></script>
+	
+</head>
+
+<body>
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+		<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			<a class="navbar-brand" href="#">
+				
+					<img  style="margin-top: -5px;" width="130" height="33" src="assets/image/logo2.png">
+				
+			</a>
+			</div>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+			
+				<div id="navbar" class="collapse navbar-collapse">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="index.html">Home <span class="sr-only">(current)</span></a></li>
+						<li><a href="browse-contract.php">Browse Contract</a></li>
+						<li><a href="faq.php">FAQ</a></li>
+						<li><a href="about.html">The Team</a></li>
+						<li><a href="contact.html">Contact Us</a></li>
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						<li><button type="submit" style="margin-left:10px;  margin-top: 10px; margin-right: 15px;"class="btn-signup">Signup</button></li>
+						<li>
+							<button type="submit" style="margin-left:10px; margin-top: 10px;"class="btn-login">login</button></li>
+						</li>
+					</ul>
+				</div><!-- /.navbar-collapse --------->
+			
+		</div> 
+	</nav>
+	
+	<!-- / Container----------------------->
+	
+	<div class="container">
+		<div class="row">
+			<div class="jumbotron" style="background: #fff;">
+			</div>
+			<div class="col-md-12 col-sm-12 db-head">
+				<h2>Browse Contract</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod<br>
+									tempor incididunt ut labore et dolore magna aliqua. Ut enim</p>
+			</div>
+			
+			<div class="col-md-12 col-sm-12">
+				<h4 style="font-family: 'Roboto', sans-serif; font-weight: 300;">Ketenagakerjaan</h4>
+			</div>
+			
+			<div class="col-md-12 col-sm-12 col-xs-12 template-ctr">
+				<div class="col-md-3 col-sm-12 template-scr-wrap">
+					<div class="col-md-12 col-sm-12 tenagakerja-templates">
+						<div class="col-md-4">
+						<img src="assets/image/ketenagakerjaan.png">
+						</div>
+						<div class="col-md-8">
+						<h5>Perjanjian Karyawan Kontrak</h5>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+						<button type="button" class="btn btn-primary btn-select">select</button>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-12 template-ctr-wrap">
+					<div class="col-md-12 col-sm-12 tenagakerja-templates">
+						<div class="col-md-4">
+						<img src="assets/image/ketenagakerjaan.png">
+						</div>
+						<div class="col-md-8">
+						<h5>Perjanjian Karyawan Kontrak</h5>
+						
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+						<button type="button" class="btn btn-primary btn-select">select</button>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-12 template-ctr-wrap">
+					<div class="col-md-12 col-sm-12 tenagakerja-templates">
+						<div class="col-md-4">
+						<img src="assets/image/ketenagakerjaan.png">
+						</div>
+						<div class="col-md-8">
+						<h5>Perjanjian Karyawan Kontrak</h5>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+						<button type="button" class="btn btn-primary btn-select">select</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-md-12 recently">
+				<h4 style="font-family: 'Roboto', sans-serif; font-weight: 300;">Kerahasiaan dan Non-Kompetisi</h4>
+			</div>
+			
+			<div class="col-md-12 col-sm-12 col-xs-12  template-scr">
+				<div class="col-sm-6 col-xs-12 template-scr-wrap">
+					<div class="col-md-4 rahasia-templates">
+						<img src="assets/image/kerahasiaan.png">
+					</div>
+					<div class="col-md-8 secret-templates-caption">
+						<h5>Perjanjian Karyawan Kontrak</h5>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+						<button type="button" class="btn btn-primary btn-select">select</button>
+					</div>
+				</div>
+				<div class="col-sm-6 col-xs-12 template-scr-wrap">
+					<div class="col-md-4 rahasia-templates">
+						<img src="assets/image/kerahasiaan.png">
+					</div>
+					<div class="col-md-8 secret-templates-caption">
+						<h5>Perjanjian Karyawan Kontrak</h5>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+						<button type="button" class="btn btn-primary btn-select">select</button>
+					</div>
+				</div>
+				<div class="col-sm-6 col-xs-12 template-scr-wrap">
+					<div class="col-md-4 rahasia-templates">
+						<img src="assets/image/kerahasiaan.png">
+					</div>
+					<div class="col-md-8 secret-templates-caption">
+						<h5>Perjanjian Karyawan Kontrak</h5>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+						<button type="button" class="btn btn-primary btn-select">select</button>
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-md-12 recently">
+				<h4 style="font-family: 'Roboto', sans-serif; font-weight: 300;">Pinjam Meminjam</h4>
+			</div>
+			
+			<div class="col-md-12 col-sm-12 col-xs-12  template-ctr">
+				<div class="col-sm-6 col-xs-12 template-ctr-wrap">
+					<div class="col-md-4 tenagakerja-templates">
+						<img src="assets/image/ketenagakerjaan.png">
+					</div>
+					<div class="col-md-8 employee-templates-caption">
+						<h5>Perjanjian Karyawan Kontrak</h5>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+						<button type="button" class="btn btn-primary btn-select">select</button>
+					</div>
+				</div>
+				<div class="col-sm-6 col-xs-12 template-ctr-wrap">
+					<div class="col-md-4 tenagakerja-templates">
+						<img src="assets/image/ketenagakerjaan.png">
+					</div>
+					<div class="col-md-8 employee-templates-caption">
+						<h5>Perjanjian Karyawan Kontrak</h5>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+						<button type="button" class="btn btn-primary btn-select">select</button>
+					</div>
+				</div>
+				<div class="col-sm-6 col-xs-12 template-ctr-wrap">
+					<div class="col-md-4 tenagakerja-templates">
+						<img src="assets/image/ketenagakerjaan.png">
+					</div>
+					<div class="col-md-8 employee-templates-caption">
+						<h5>Perjanjian Karyawan Kontrak</h5>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+						<button type="button" class="btn btn-primary btn-select">select</button>
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-md-12 recently">
+				<h4 style="font-family: 'Roboto', sans-serif; font-weight: 300;">Sewa Menyewa</h4>
+			</div>
+			
+			<div class="col-md-12 col-sm-12 col-xs-12  template-ctr">
+				<div class="col-sm-6 col-xs-12 template-ctr-wrap">
+					<div class="col-md-4 tenagakerja-templates">
+						<img src="assets/image/ketenagakerjaan.png">
+					</div>
+					<div class="col-md-8 employee-templates-caption">
+						<h5>Perjanjian Karyawan Kontrak</h5>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+						<button type="button" class="btn btn-primary btn-select">select</button>
+					</div>
+				</div>
+				<div class="col-sm-6 col-xs-12 template-ctr-wrap">
+					<div class="col-md-4 tenagakerja-templates">
+						<img src="assets/image/ketenagakerjaan.png">
+					</div>
+					<div class="col-md-8 employee-templates-caption">
+						<h5>Perjanjian Karyawan Kontrak</h5>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+						<button type="button" class="btn btn-primary btn-select">select</button>
+					</div>
+				</div>
+				<div class="col-sm-6 col-xs-12 template-ctr-wrap">
+					<div class="col-md-4 tenagakerja-templates">
+						<img src="assets/image/ketenagakerjaan.png">
+					</div>
+					<div class="col-md-8 employee-templates-caption">
+						<h5>Perjanjian Karyawan Kontrak</h5>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+						<button type="button" class="btn btn-primary btn-select">select</button>
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-md-12 col-sm-12 col-xs-12" style="border-bottom: 2px solid #eee; height: 50px;">
+			
+			</div>
+			
+		</div>
+	</div>
+	
+	<div class="row">
+	<div id="footer">
+				<div class ="container" style="border-bottom: 2px solid #fff">
+					<div class="col-md-4 column footer-about">
+						<table class="ft-contact-tb">
+							<tr class="tr-logo">
+								<td >
+									<a class="footer-td">
+						
+										<img src="assets/image/footer-info.png">
+							
+										About Us
+									</a>
+								</td>
+							</tr>
+						<tr>
+							<td>
+								<img class="img-foot-logo" src="assets/image/logo-3.png" width="250" height="50">
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+									eiusmod tempor incididunt ut labore et dolore maqna aliqua. Ut
+									enim ad minim veniam, quis nostrud exercitation ullamco
+									laboris nisi ut aliquip ex ea commodo consequat.</p>
+							</td>
+						</tr>
+						</table>
+					
+					</div>
+				
+					<div class="col-md-4 column footer-contact">
+						<table class="ft-contact-tb">
+						<tr>
+							<td>
+								<a class="footer-td">
+									<img  style="float: left;" width="32" height="32" src="assets/image/footer-mail.png">
+									Contact
+								</a>
+							</td>
+						</tr>
+						<tr>
+							<td class="responsive-footer-contact">
+							
+								<img width="16" height="16" src="assets/image/pin-loc.png">
+								<p class="footer-td">
+								PT Privi Teknologi Ventura<br>
+								Jalan Rejowinangun Perumahan<br>
+								Gedong Kuning Asri no.27,<br>
+								Kota Gede, Yogyakarta 55171.
+								</p>
+							</td>
+						</tr>
+						<tr>
+							<td class="responsive-footer-contact">
+						
+								<img width="16" height="16" src="assets/image/e-mail16.png">
+							
+								<p class="footer-td">info@privydoc.com</p>
+						
+							</td>
+						</tr>
+					
+						<tr>
+							<td class="responsive-footer-contact">
+						
+								<img src="assets/image/footer-phone.png">
+							
+								<p class="footer-td">+62 274 4544668 </p>
+						
+							</td>
+						</tr>
+					
+						</table>
+					</div>
+				
+					<div class="col-md-4 column footer-link">
+						<table class="col-md-12">
+							<tr class="tr-logo">
+								<td>
+									<a class="footer-td">
+										<img  style="float: left;" width="32" height="32" src="assets/image/footer-link.png">
+										Link
+									</a>
+								</td>
+							</tr>
+						
+							<tr>
+								<td>
+									<div class="link-list">
+										<img width="10" height="10" src="assets/image/box.png">
+										<a>PrivyDoc Blog</a>
+									</div>	
+									<div class="link-list">
+										<img width="10" height="10" src="assets/image/box.png">
+										<a>FAQ</a>
+									</div>
+									<div class="link-list">
+										<img width="10" height="10" src="assets/image/box.png">
+										<a>Term of Use</a>
+									</div>
+									<div class="link-list">
+										<img width="10" height="10" src="assets/image/box.png">
+										<a>PrivyGet</a>
+									</div>
+								</td>
+							</tr>
+						</table>
+					</div>
+				
+				</div>
+				<div class="ends-footer">
+					<div class="right-reserved">
+						<h6>Copyright &copy; 2015 PT. Privi Teknologi Ventura (PriviGate). All Rights Reserved</h6>
+					</div>
+				
+					<div class="footer-media">
+					
+						<div class="img">
+							<a target="_blank" href="#"><img src="assets/image/facebook.png" alt="facebook" width="32" height="32"></a>
+							<a target="_blank" href="#"><img src="assets/image/twitter.png" alt="twitter" width="32" height="32"></a>
+							<a target="_blank" href="#"><img src="assets/image/g+.png" alt="googleplus" width="32" height="32"></a>
+							<a target="_blank" href="#"><img src="assets/image/youtube.png" alt="linkedin" width="32" height="32"></a>
+						</div>
+					
+					</div>
+				
+				</div>
+			</div>
+	</div>
+	<script>
+		$(document).ready(function(){
+		$(".dropdown-toggle").dropdown();
+		});
+	</script>
+	
+	
+	
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
+
---
+++ faq.php
@@ -0,0 +1,252 @@
+<!doctype HTML>
+<html lang="en">
+
+<head>
+	
+	<meta charset="UTF-8">
+	<title>Privydoc</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<link rel="stylesheet" href="css/bootstrap.css" media="screen">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css" media="screen">
+	<link href='http://fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Roboto:100' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Exo+2:400,300' rel='stylesheet' type='text/css'>
+	<link rel="shortcut icon" href="assets/image/favicon.ico" sizes="16x16" />
+	<script src="js/jquery.js"></script>
+	
+</head>
+
+<body>
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+		<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			<a class="navbar-brand" href="index.html">
+				
+					<img  width="130" height="33" src="assets/image/logo2.png">
+				
+			</a>
+			</div>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+			<div id="navbar-plus">
+				<div id="navbar" class="collapse navbar-collapse">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="index.html">Home <span class="sr-only">(current)</span></a></li>
+						<li><a href="browse-contract.php">Browse Contract</a></li>
+						<li><a href="faq.php">FAQ</a></li>
+						<li><a href="about.html">The Team</a></li>
+						<li><a href="contact.html">Contact Us</a></li>
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						<li><button type="submit" style="margin-top: 5px;"class="btn btn-warning btn-lg">Sign Up Free</button></li>
+						<li>
+							<a style="margin-top: 5px;"href="#">Login</a>
+						</li>
+					</ul>
+				</div><!-- /.navbar-collapse --------->
+			</div>
+		</div> 
+	</nav>
+	
+	<!-- / Container----------------------->
+	<div class="container">
+		<div class="row-clearfix">
+			<div class="jumbotron" style="background: transparent;">
+			</div>
+			<div class="col-md-12 col-sm-12 col-xs-12 page-header">
+				<h3>FAQ PrivyDoc</h3>
+			</div>
+			<div class="col-md-12 col-sm-12 col-xs-12 faq">
+				<div class="faq-content">
+					<div class="faq-container">
+
+						<div class="col-md-12 col-sm-12 col-xs-12 faq-list">
+							<label for="q1">Q: Lorem ipsum dolor sit amet, consectetur adipisicing elit? </label> <input name="question" id="q1" type="checkbox" value="">
+							<div class="answer">
+								<p>Maecenas justo erat, consectetur vel pellentesque quis, pharetra id tellus. Morbi ut nibh nisi. Vestibulum porta sollicitudin erat et convallis. u</p>
+							</div>
+						</div>
+
+						<div class="col-md-12 col-sm-12 col-xs-12 faq-list">
+							<label for="q2">Q: Consectetur vel pellentesque quis? </label> <input name="question" id="q2" type="checkbox" value="">
+							<div class="big">
+								<p>Maecenas justo erat, consectetur vel pellentesque quis, pharetra id tellus. Morbi ut nibh nisi. Vestibulum porta sollicitudin erat et convallis. u</p>
+								<p>Maecenas justo erat, consectetur vel pellentesque quis, pharetra id tellus. Morbi ut nibh nisi. Vestibulum porta sollicitudin erat et convallis. u</p>
+							</div>
+						</div>
+
+						<div class="col-md-12 col-sm-12 col-xs-12 faq-list">
+							<label for="q3">Q: Morbi ut nibh nisi? </label> <input name="question" id="q3" type="checkbox" value="">
+							<div class="answer">
+								<p>Maecenas justo erat, consectetur vel pellentesque quis, pharetra id tellus. Morbi ut nibh nisi. Vestibulum porta sollicitudin erat et convallis. u</p>
+							</div>
+						</div>
+
+						<div class="col-md-12 col-sm-12 col-xs-12 faq-list">
+							<label for="q4">Q: Vestibulum porta sollicitudin erat? </label> <input name="question" id="q4" type="checkbox" value="">
+							<div class="answer">
+								<p>Maecenas justo erat, consectetur vel pellentesque quis, pharetra id tellus. Morbi ut nibh nisi. Vestibulum porta sollicitudin erat et convallis. u</p>
+							</div>
+						</div>
+				</div>
+				</div>
+			</div>
+			<div class="col-md-12 col-sm-12 col-xs-12" style="height: 100px; margin-top: 20px; border-top: 2px solid #eee">
+		</div>
+		</div>
+		
+	</div>		
+	
+	
+	
+	
+	<!-----/ #footer----------------------->
+	<div class="row">
+		<div id="footer">
+				<div class ="container" style="border-bottom: 2px solid #fff">
+					<div class="col-md-4 column footer-about">
+						<table class="ft-contact-tb">
+							<tr class="tr-logo">
+								<td >
+									<a class="footer-td">
+						
+										<img src="assets/image/footer-info.png">
+							
+										About Us
+									</a>
+								</td>
+							</tr>
+						<tr>
+							<td>
+								<img class="img-foot-logo" src="assets/image/logo-3.png" width="250" height="50">
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+									eiusmod tempor incididunt ut labore et dolore maqna aliqua. Ut
+									enim ad minim veniam, quis nostrud exercitation ullamco
+									laboris nisi ut aliquip ex ea commodo consequat.</p>
+							</td>
+						</tr>
+						</table>
+					
+					</div>
+				
+					<div class="col-md-4 column footer-contact">
+						<table class="ft-contact-tb">
+						<tr>
+							<td>
+								<a class="footer-td">
+									<img  style="float: left;" width="32" height="32" src="assets/image/footer-mail.png">
+									Contact
+								</a>
+							</td>
+						</tr>
+						<tr>
+							<td class="responsive-footer-contact">
+							
+								<img width="16" height="16" src="assets/image/pin-loc.png">
+								<p class="footer-td">
+								PT Privi Teknologi Ventura<br>
+								Jalan Rejowinangun Perumahan<br>
+								Gedong Kuning Asri no.27,<br>
+								Kota Gede, Yogyakarta 55171.
+								</p>
+							</td>
+						</tr>
+						<tr>
+							<td class="responsive-footer-contact">
+						
+								<img width="16" height="16" src="assets/image/e-mail16.png">
+							
+								<p class="footer-td">info@privydoc.com</p>
+						
+							</td>
+						</tr>
+					
+						<tr>
+							<td class="responsive-footer-contact">
+						
+								<img src="assets/image/footer-phone.png">
+							
+								<p class="footer-td">+62 274 4544668 </p>
+						
+							</td>
+						</tr>
+					
+						</table>
+					</div>
+				
+					<div class="col-md-4 column footer-link">
+						<table class="col-md-12">
+							<tr class="tr-logo">
+								<td>
+									<a class="footer-td">
+										<img  style="float: left;" width="32" height="32" src="assets/image/footer-link.png">
+										Link
+									</a>
+								</td>
+							</tr>
+						
+							<tr>
+								<td>
+									<div class="link-list">
+										<img width="10" height="10" src="assets/image/box.png">
+										<a>PrivyDoc Blog</a>
+									</div>	
+									<div class="link-list">
+										<img width="10" height="10" src="assets/image/box.png">
+										<a>FAQ</a>
+									</div>
+									<div class="link-list">
+										<img width="10" height="10" src="assets/image/box.png">
+										<a>Term of Use</a>
+									</div>
+									<div class="link-list">
+										<img width="10" height="10" src="assets/image/box.png">
+										<a>PrivyGet</a>
+									</div>
+								</td>
+							</tr>
+						</table>
+					</div>
+				
+				</div>
+				<div class="ends-footer">
+					<div class="right-reserved">
+						<h6>Copyright &copy; 2015 PT. Privi Teknologi Ventura (PriviGate). All Rights Reserved</h6>
+					</div>
+				
+					<div class="footer-media">
+					
+						<div class="img">
+							<a target="_blank" href="#"><img src="assets/image/facebook.png" alt="facebook" width="32" height="32"></a>
+							<a target="_blank" href="#"><img src="assets/image/twitter.png" alt="twitter" width="32" height="32"></a>
+							<a target="_blank" href="#"><img src="assets/image/g+.png" alt="googleplus" width="32" height="32"></a>
+							<a target="_blank" href="#"><img src="assets/image/youtube.png" alt="linkedin" width="32" height="32"></a>
+						</div>
+					
+					</div>
+				
+				</div>
+			</div>
+	</div>
+	
+	
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
+
